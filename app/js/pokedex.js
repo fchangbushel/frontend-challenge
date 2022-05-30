581aa67f5fee01b5bb8f2e5446f560bf2d6ad207
Feng Chang <fchang@bushelpowered.com>
2022-05-30 00:27:08 -0500

    showing pokemon list

--- app/js/pokedex.js
+++ app/js/pokedex.js
@@ -1,10 +1,32 @@
-const fetchPokemon = () => {
+const pokedex = document.getElementById("pokedex");
+
+const fetchPokemons = () => {
     const url = `https://intern-pokedex.myriadapps.com/api/v1/pokemon`;
-    fetch(url).then( res => {
+    const pokemons = [];
+
+    fetch(url).then((res) => {
         return res.json();
-    }).then( data => {
-        console.log(data);
-    })
+    }).then((result) => {
+            const pokemon = result.data.map((data) => ({
+            id: data.id,
+            name: data.name,
+            image: data.image,
+            types: data.types
+        }));
+        displayPokemon(pokemon);
+    });
 }
 
-fetchPokemon();
+const displayPokemon = (pokemon) => {
+    console.log(pokemon);
+    const pokemonHTMLString = pokemon.map ( pokeman => `
+    <li class="card">
+        <h2 class="card-title">${pokeman.name}</h2>
+        <img class="card-image" src="${pokeman.image}">
+        <p class="card-types">${pokeman.types}</p>
+    </li>
+    `).join('');
+    pokedex.innerHTML = pokemonHTMLString;
+}
+
+fetchPokemons();
